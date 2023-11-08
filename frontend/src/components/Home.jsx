ec2f3a3e6dc03c7d5d45c37a8640dd89e69d5f48
tianhaobaoq <xigaoang@gmail.com>
2023-11-08 12:12:16 +0100

    Commit2

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,67 +1,91 @@
 //Importamos el useSelector del react-redux
 import { useSelector } from 'react-redux'
-import { AppBar, Typography, Container, Grid, Link, Toolbar,} from '@mui/material'
+import { AppBar, Typography, Container, Grid, Toolbar,} from '@mui/material'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,Link} from 'react-router-dom'
 import Button from '@mui/material/Button';
 import { useDispatch } from 'react-redux';
 import { loginActions } from '../store/storelogin';
-import AdbIcon from '@mui/icons-material/Adb';
+import AttractionsIcon from '@mui/icons-material/Attractions';
 
 function Home() {
-
-//Almacenamos en la variable userData el estado del store
-const userData = useSelector(state => state.login)
-const usuario = userData.usuario
-
-//Trozo de código donde vamos a usar el useEffect(): siempre los hooks van al principio del componente
-const isLoggedin = userData.isAutenticated
-const navigate = useNavigate()
-useEffect(() => {
-if (!isLoggedin) {
-navigate('/')
-}
-}, [isLoggedin, navigate])
-
-
-//Función para el boton
-const dispatch = useDispatch();
-const CerrarSesion = () => {
-    dispatch(loginActions.logout())
-    navigate('/')    
-}
-
-
-//Comprobamos por la consola qué obtenemos en userData
-console.log(userData)
-
-return <>
-    <AppBar position='static'>
-        <Container>
+    const userData = useSelector(state => state.login)
+    const isLoggedin = userData.isAutenticated
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      if (!isLoggedin) {
+        navigate('/')
+      }
+    }, [isLoggedin, navigate])
+  
+    const CerrarSesion = () => {
+      dispatch(loginActions.logout())
+      navigate('/')    
+    }
+  
+    return (
+      <>
+        <AppBar position='static'>
+          <Container>
             <Toolbar>
-                <Grid container>
-                    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-                    //AdbIcon es un componente de la librería '@mui/icons-material/Adb' Elige uno diferente
-                        <AdbIcon/>
-                        <Typography>{userData.usuario}</Typography>
-                        </Grid>
-                        <Grid item xs={12} md={4} lg={3}>
-                            <Link to='/home'>Inicio</Link>
-                        </Grid>
-                        AÑADIR TANTOS GRID ITEMS COMO SEAN NECESARIOS CON LOS LINKS
-                        A LAS PÁGINAS Informes y Ayuda
-                        <Grid item xs={12} md={4} lg={3}>
-                        <Button variant="contained" onClick = {CerrarSesion}>Salir</Button>
-                    </Grid>
+              <Grid container justifyContent="space-between">
+                <Grid item container xs={3} sm={3} md={3} lg={2} xl={2} sx={{alignContent:'center'}}>
+                  <AttractionsIcon />
+                  <Typography sx={{ display: 'inline', fontSize: '24px' }}>{userData.userName}</Typography>
                 </Grid>
+  
+                <Grid item container xs={3} md={3} lg={2} xl={2} sx={{alignContent:'center'}}>
+                  <Link to='/home'>Inicio</Link>
+                </Grid>
+  
+                <Grid item container xs={3} md={3} lg={2} xl={2} sx={{alignContent:'center'}}>
+                  <Link to='/informes'>Informes</Link>
+                </Grid>
+  
+                <Grid item container xs={3} md={3} lg={2} xl={2} sx={{alignContent:'center'}}>
+                  <Link to='/ayuda'>Ayuda</Link>
+                </Grid>
+  
+                <Grid item container xs={3} md={3} lg={2} xl={3} justifyContent="flex-end">
+                  <Button variant="contained" color="secondary" onClick={CerrarSesion}>Salir</Button>
+                </Grid>
+              </Grid>
             </Toolbar>
-        </Container>
-    </AppBar>
+          </Container>
+        </AppBar>
+
+        <Paper AÑADIR PROPIEDADES>
+            <Box component='form' autoComplete='off' onSubmit={handleSaveItem} >
+                <Grid container AÑADIR PROPIEDADES>
+                    <Grid item xs={X} md={X}>
+                        <TextField
+                        label='Nombre'
+                        required
+                        value={item.nombre}
+                        /*Cuando el usuario escriba algo en el TextField nombre, se irá almacenando en el
+                        atributo nombre del objeto item*/
+                        onChange={(event) => setItem({...item, nombre: event.target.value })}
+                        >
+                        </TextField>
+                    </Grid>
+                    //AÑADIR LOS DEMÁS ELEMENTOS DEL FORMULARIO: los TextField y el Button
+                    //El Button debe de ser de type='submit' porque es un formulario
+                </Grid>
+            </Box>
+        </Paper>
 
 
 
-</>
 
-}
 
-export default Home
+
+
+      </>
+    );
+  }
+  
+  export default Home;
+  
+  
