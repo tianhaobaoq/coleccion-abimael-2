5903cd95d62d3706ee4944a4b9a33d67d705e65a
tianhaobaoq <127838303+tianhaobaoq@users.noreply.github.com>
2023-11-22 11:20:41 +0100

    Commit

---
+++ frontend/src/components/InformeColeccion.jsx
@@ -0,0 +1,61 @@
+
+import React from "react";
+import MaterialTable from "@material-table/core";
+import { ExportCsv, ExportPdf } from "@material-table/exporters";
+import { blue } from "@mui/material/colors";
+
+function InformesColeccion(props) {
+    
+
+    const col = [
+        { title: "Nombre", field: "nombre",filtering:false},
+        { title: "Marca", field: "marca" },
+        { title: "Tipo", field: "tipo" },
+        { title: "Precio", field: "precio",filtering:false}
+        ];
+    
+    console.log(props)
+
+
+    return (<>
+
+        <MaterialTable
+            columns={col} data={props.AAA}
+            title="Tabla"
+           
+            renderSummaryRow = {({column,data}) =>
+            column.field === 'precio'
+            ?{
+                value:data.reduce((agg,row) => agg + row.precio,0),
+                style: { background: "#06290B"},
+            }
+            :undefined
+            }
+
+            //Esto de aquí abajo es lo nuevo. Dentro de options pondremos la parte de exportación
+            options={{
+                exportMenu: [
+                {
+                label: "Export PDF",
+                exportFunc: (cols, datas) => ExportPdf(cols, datas, "myPdfFileName"),
+                },
+                {
+                label: "Export CSV",
+                exportFunc: (cols, datas) => ExportCsv(cols, datas, "myCsvFileName"),
+                },
+                {
+                    headerStyle: {
+                        backgroundColor: blue,
+                        color: blue
+                       }
+                }
+                ],
+                draggable: true,
+                    columnsButton: true,
+                    filtering: true
+            }}
+    />
+    </>)
+
+}
+export default InformesColeccion
--- frontend/src/components/informes.jsx
+++ frontend/src/components/informes.jsx
@@ -1,11 +1,63 @@
+import { Grid,Paper} from '@mui/material'
 import TopBar from "./TopBar";
+import Button from '@mui/material/Button';
+import InformesColeccion from './InformeColeccion';
+import { useEffect,useState} from 'react'
+import { useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+
 
 function Informes() {
-    return(
-        <>
+
+  const navigate = useNavigate()
+  const userData = useSelector(state => state.login)
+  const isLoggedin = userData.isAutenticated
+
+  const [TableData,setTableData] = useState('')
+  const [Datos,setDatos] = useState(false)
+
+  useEffect(() => {
+    if (!isLoggedin) {
+      navigate('/')
+    }else{
+      fetch(`http://localhost:3030/getItem`)
+      .then(response => response.json())
+      .then(response => {
+          console.log(response)
+          if(Object.keys(response.data).length !==0) {
+              setTableData(response.data)
+          }
+      })
+    }
+  }, [isLoggedin, navigate])
+
+  const data = (e) => {
+    setDatos(true)
+    console.log(Datos)
+  }
+
+
+    return <>
         <TopBar/>
+        
+        <Paper>
+        <Grid container>
+                <Grid item xs={5} md={5}/>
+                  <Grid item xs={3} md={3} marginTop={"10px"} marginBottom={"10px"}>
+                    <Button variant="contained" color="secondary" onClick={data}>INFORME COLECCION</Button>
+                  </Grid>
+        </Grid>
+        </Paper>
+
+        <Grid container>
+                  <Grid item xs={12} md={12} marginTop={"10px"} marginBottom={"10px"}>
+                  {Datos && <InformesColeccion AAA = {TableData}/>}
+                  </Grid>
+        </Grid>
+
+
         </>
-    );
 }
 
 export default Informes
